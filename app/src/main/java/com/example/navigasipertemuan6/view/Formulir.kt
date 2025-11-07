29f766d3516cc44c49c21092554c9f5ddf80fc4d
adilaroisa <adilaroisa688@gmail.com>
2025-11-07 10:19:17 +0700

    Menambahkan tombol Submit

--- app/src/main/java/com/example/navigasipertemuan6/view/Formulir.kt
+++ app/src/main/java/com/example/navigasipertemuan6/view/Formulir.kt
@@ -84,6 +84,13 @@ fun FormIsian(
                 label = {Text(text = "Alamat")},
                 onValueChange = {}
             )
+            Spacer(modifier = Modifier.height(30.dp))
+            Button(
+                modifier = Modifier.fillMaxWidth(1f),
+                onClick = OnSubmitBtnClick
+            ) {
+                Text(stringResource(R.string.submit))
+            }
         }
     }
 }
