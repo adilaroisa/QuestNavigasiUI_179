092d37dac1e8e78875e93110b92d1777b1b496f4
adilaroisa <adilaroisa688@gmail.com>
2025-11-07 14:46:20 +0700

    menambahkan data dummy/konstanta untuk tampilan

--- app/src/main/java/com/example/navigasipertemuan6/view/TampilData.kt
+++ app/src/main/java/com/example/navigasipertemuan6/view/TampilData.kt
@@ -1,7 +1,15 @@
 package com.example.navigasipertemuan6.view
 
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.runtime.Composable
+
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun TampilData() {
     onBackBtnClick: () -> Unit
-}
+) {
+        val items = listOf(
+            Pair(first = stringResource(id = R.string.nama_lengkap), second = "Contoh Nama"),
+            Pair(first = stringResource(id = R.string.jenis_kelamin), second = "Lainnya"),
+            Pair(first = stringResource(id = R.string.alamat), second = "Yogyakarta")
+        )
