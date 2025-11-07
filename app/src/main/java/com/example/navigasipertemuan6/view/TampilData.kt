cec0802c8435bcd9c4b19690e367339fccfa484d
adilaroisa <adilaroisa688@gmail.com>
2025-11-07 14:58:43 +0700

    Menambahkan tombol 'Back' di bawah

--- app/src/main/java/com/example/navigasipertemuan6/view/TampilData.kt
+++ app/src/main/java/com/example/navigasipertemuan6/view/TampilData.kt
@@ -43,3 +43,13 @@ fun TampilData() {
                         }
                     }
                 }
+                Spacer(modifier = Modifier.height(height = 10.dp))
+                Button(
+                    modifier = Modifier.fillMaxWidth(),
+                    onClick = onBackBtnClick
+                ) {
+                    Text(text = stringResource(id = R.string.back))
+                }
+            }
+        }
+    }
